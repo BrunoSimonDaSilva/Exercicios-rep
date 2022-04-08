ba9d23896eb9ada915393b2ea10247bbe156b3c4
BrunoSimonDaSilva <96893269+BrunoSimonDaSilva@users.noreply.github.com>
2022-04-08 19:44:01 -0300

    Add files via upload

--- scripto.js
+++ scripto.js
@@ -16,7 +16,7 @@ function atividade1() {
     while (n1 < 0 || n1 > 10) {
         n1 = Number(prompt("digite um valor valido"));
     }
-    resp1.textContent = ("O valor Digitador é " + n1);
+    resp1.textContent = ("O valor Digitado é " + n1);
 
 }
 
@@ -31,12 +31,26 @@ function atividade2() {
 }
 
 function atividade3() {
-    let resp3 = document.getElementById(3);
+    let resp3 = document.getElementById(1515);
     let nome = prompt("Digite seu nome: ");
     let idade = Number(prompt("Digite sua idade: entre 0 à 150: "));
-    while(nome <= nome.length){
+    let salario = Number(prompt("Digite o valor do seu salario: "));
+    let sexo = prompt("Digite o sexo biológico: ");
+    let  Estciv = prompt("Digite seu  Estado Civil: ");
+    while(nome < nome.length){
         nome = prompt("Digite um nome acima de 3 caracteres: ");
     }
-    
-
+    while(idade <= 0 || idade >= 150){
+        idade = Number(prompt("Digite sua idade: ENTRE 0 à 150: "));
+    }
+    while(salario <= 0){
+        salario = Number(prompt(" Error, valor deve ser maior que 0: "));
+    }
+    while(sexo.toLocaleLowerCase != "f" || sexo.toLocaleLowerCase != "m"){
+        sexo = prompt("Digite o sexo BIOLÓGICO: ");
+    }
+    while(Estciv != 's' || Estciv != 'c' || Estciv != 'v' || Estciv != 'd'){
+        Estciv = prompt("Valor inválido digite seu estádo civil");
+    }
+    resp3.textContent(`"Nome é ${nome}, sua idade é ${idade}, o salário é R$${salario}, o sexo é ${sexo.toLocaleUpperCase()}, estado civíl ${Estciv} "`)
 }
